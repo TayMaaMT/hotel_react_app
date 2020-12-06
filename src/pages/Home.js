a4b0eb3399c03293b9a4df51d7c7fec8ff6328ac
taymaa alzayyan <taymaa.2917@gmail.com>
2020-12-06 05:50:23 +0200

    complete about and cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 import Header from "../components/home/Header";
+import About from "../components/home/About";
+import Rooms from "../components/home/Rooms";
 const Home = () => {
   return (
     <>
       <Header />
+      <About />
+      <Rooms/>
     </>
   );
 };
